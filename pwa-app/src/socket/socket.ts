40444806c1a3bac6cbbd328c2fb334f3304e4565
Prashant Rajput <145877039+prshntrajput@users.noreply.github.com>
2025-07-09 14:24:57 +0530

    Update socket.ts

--- pwa-app/src/socket/socket.ts
+++ pwa-app/src/socket/socket.ts
@@ -4,7 +4,7 @@ let socket: Socket;
 
 export const initSocket = () => {
   if (!socket) {
-    socket = io('http://localhost:8000');
+    socket = io(process.env.NEXT_PUBLIC_API_URL);
   }
   return socket;
 };
